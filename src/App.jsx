4cd585202f170e13d79abd4dcf260bdf619c94c9
Federico Stempler <zackmango@gmail.com>
2023-08-16 14:47:44 -0300

    memo Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
 import { useEffect, useState } from 'react';
 import Board from './components/Board/Board';
-const emojiList = [...'🍋🎗️💰🐭🌄👾🦸‍♀️🌙👴🏻🌜🙈🍉🔰'];
+const emojiList = ['🍋', '🎗️', '💰', '🐭', '🌄', '👾', '🦸‍♀️', '🌙', '🌜', '🙈', '🍉', '🔰']
+;
 import './App.css'
 
 
 function App() {
-  const [shuffledMemoBlocks, setShuffledMemoBlocks] = useState([]);
-  const [selectedMemoBlock, setSelectedMemoBlock] = useState(null);
-  const [animating, setAnimating] = useState(false);
+
+  //Estamos para almacenar lso bloques, el bloque seleccionado y si tiene una animación. 
+  const [shuffledMemoBlocks, setShuffledMemoBlocks] = useState([]); //Almacena los bloques en la memoria
+  const [selectedMemoBlock, setSelectedMemoBlock] = useState(null); //Almacena los bloques que selecciona el usuario
+  const [animating, setAnimating] = useState(false); //Almacena los bloques seleccionados en el tablero
 
   useEffect( () => {
     const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]);
     setShuffledMemoBlocks(shuffledEmojiList.map( (emoji, i) => ({ index: i, emoji, flipped: false}) ));
   }, []);
 
-  const shuffleArray = a => {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
+  //Algoritmo Fisher-Yates
+  const shuffleArray = a => { //se pasa el array (a) como argumento
+    for (let i = a.length - 1; i > 0; i--) { //Itera desde el último elemento del array hacia el primero
+        const j = Math.floor(Math.random() * (i + 1)); //Genera un número aleatorio entre 0 y 1
+        [a[i], a[j]] = [a[j], a[i]];//Intercamvia los elementos en las posiciones i y j desestructurando el array
     }
-    return a;
+    return a; //devuelve el array mezclado
   }
 
   const handleMemoClick = memoBlock => {
-    const flippedMemoBlock = {...memoBlock, flipped: true };
+    const flippedMemoBlock = {...memoBlock, flipped: true }; //Cambia el estado del bloque cuando se hace click y lo voltea
     let shuffledMemoBlocksCopy = [...shuffledMemoBlocks];
     shuffledMemoBlocksCopy.splice(memoBlock.index, 1, flippedMemoBlock);
     setShuffledMemoBlocks(shuffledMemoBlocksCopy);
